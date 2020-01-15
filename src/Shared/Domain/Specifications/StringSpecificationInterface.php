2cb445810f01349bee8d8ea27cf37413576ad2d5
Fernando Aparicio <faparicior@gmail.com>
2020-01-15 22:07:10 +0100

    #3 SignUp User: clean code

--- src/Shared/Domain/Specifications/StringSpecificationInterface.php
+++ src/Shared/Domain/Specifications/StringSpecificationInterface.php
@@ -2,10 +2,8 @@
 
 namespace App\Shared\Domain\Specifications;
 
-use App\Users\User\Domain\Specifications\StringMoreThanSevenCharacters;
 
 interface StringSpecificationInterface
 {
-
     public function isSatisfiedBy(string $data);
 }
