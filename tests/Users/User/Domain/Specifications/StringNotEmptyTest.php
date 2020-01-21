2a56abb2a7f1a5304eab7902dc979d4ed813e02d
Fernando Aparicio <faparicior@gmail.com>
2020-01-21 20:33:57 +0100

    #3 SignUp User: improve tests

---
+++ tests/Users/User/Domain/Specifications/StringNotEmptyTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Users\User\Domain\Specifications;
+
+use App\Users\User\Domain\Specifications\StringNotEmpty;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class StringNotEmptyTest extends TestCase
+{
+    const VALID_STRING = '12345678';
+    const INVALID_STRING = '';
+
+    /**
+     * @group UnitTests
+     * @group Users
+     * @group Domain
+     */
+    public function testStringNotEmptyCannotBeInstantiatedDirectly()
+    {
+        self::expectException(\Error::class);
+
+        new StringNotEmpty();
+    }
+
+    /**
+     * @group UnitTests
+     * @group Users
+     * @group Domain
+     */
+    public function testStringNotEmptyValidationReturnsTrue()
+    {
+        $specification = StringNotEmpty::build();
+
+        self::assertTrue($specification->isSatisfiedBy(self::VALID_STRING));
+    }
+
+    /**
+     * @group UnitTests
+     * @group Users
+     * @group Domain
+     */
+    public function testStringNotEmptyValidationReturnsFalse()
+    {
+        $specification = StringNotEmpty::build();
+
+        self::assertFalse($specification->isSatisfiedBy(self::INVALID_STRING));
+    }
+}
