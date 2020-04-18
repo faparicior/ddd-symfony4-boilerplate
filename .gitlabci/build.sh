3409e16423ea38ba91f61ab1c46ca6ff4cef206b
Fernando Aparicio <faparicior@gmail.com>
2020-04-18 20:41:44 +0200

    gitlab-ci

--- .gitlabci/build.sh
+++ .gitlabci/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 composer install
+
+export APP_ENV=test
+./bin/simple-phpunit
+
 bin/console --env=behat doctrine:database:create
 bin/console --env=behat doctrine:migrations:migrate --no-interaction
+
+export APP_ENV=behat
+./bin/behat
--- .gitlabci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export APP_ENV=test
-
-./bin/simple-phpunit
-
-export APP_ENV=behat
-
-./bin/behat
