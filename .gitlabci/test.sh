2d3a2a2b528ceccbf521112a5304f82ad5cc90d7
Fernando Aparicio <faparicior@gmail.com>
2020-04-19 12:03:25 +0200

    gitlab-ci code coverage pages

--- .gitlabci/test.sh
+++ .gitlabci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export APP_ENV=test
-./bin/simple-phpunit
+./bin/simple-phpunit --config phpunit.xml --coverage-html build/coverage-report --log-junit build/junit.xml --coverage-clover build/clover.xml
 
 export APP_ENV=behat
 ./bin/behat
