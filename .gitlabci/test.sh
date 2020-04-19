5609893b5350d8b2e2e27f06df587da43617db00
Fernando Aparicio <faparicior@gmail.com>
2020-04-19 20:22:41 +0200

    code climate

--- .gitlabci/test.sh
+++ .gitlabci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export APP_ENV=test
-./bin/simple-phpunit --config phpunit.xml --coverage-html build/coverage-report --log-junit build/junit.xml --coverage-clover build/clover.xml --coverage-text --colors=never
+./bin/simple-phpunit --config phpunit.xml --coverage-html build/coverage-report --log-junit build/logs/junit.xml --coverage-clover build/logs/clover.xml --coverage-text --colors=never
 
 export APP_ENV=behat
 ./bin/behat
