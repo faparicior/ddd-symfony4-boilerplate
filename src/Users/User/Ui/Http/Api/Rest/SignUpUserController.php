61adeb1ade1dff0a90ff72d78bf56304678aed46
Fernando Aparicio <faparicior@gmail.com>
2020-05-06 17:13:14 +0200

    improve test coverage and unify api rest controller

--- src/Users/User/Ui/Http/Api/Rest/SignUpUserController.php
+++ src/Users/User/Ui/Http/Api/Rest/SignUpUserController.php
@@ -2,74 +2,11 @@
 
 namespace App\Users\User\Ui\Http\Api\Rest;
 
-use App\Shared\Domain\Exceptions\DomainException;
+use App\Shared\Ui\Http\Api\Rest\AppController;
 use App\Users\User\Application\SignUpUser\SignUpUserCommand;
-use League\Tactician\CommandBus;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
-class SignUpUserController
+class SignUpUserController extends AppController
 {
-    private CommandBus $bus;
-    private LoggerInterface $logger;
-
-    public function __construct(CommandBus $bus, LoggerInterface $logger)
-    {
-        $this->bus = $bus;
-        $this->logger = $logger;
-    }
-
-    /**
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function execute(Request $request): JsonResponse
-    {
-        try {
-            $content = $request->getContent();
-            if ($content === 'TEST_SHOULD_FAIL_WITH_500_EXCEPTION') {
-                throw new \Exception("TEST_SHOULD_FAIL_WITH_500_EXCEPTION", 500);
-            }
-
-            $data = json_decode($content, true);
-
-            if (is_null($data)) {
-                $this->logger->error("Empty data or bad json received");
-
-                return JsonResponse::create(
-                    'Empty data or bad json received',
-                    Response::HTTP_BAD_REQUEST
-                );
-            }
-
-            $response = $this->handleRequest($data);
-        } catch (DomainException $exception) {
-            $this->logger->error($exception->getMessage(), [$exception->getTraceAsString()]);
-
-            return JsonResponse::create(
-                $exception->getMessage(),
-                Response::HTTP_BAD_REQUEST
-            );
-        } catch (Throwable $exception)
-        {
-            $this->logger->error($exception->getMessage(), [$exception->getTraceAsString()]);
-
-            $message = ($_SERVER['APP_DEBUG'] === '1') ? 'Server error:'.$exception->getMessage() : '';
-            return JsonResponse::create(
-                $message,
-                Response::HTTP_INTERNAL_SERVER_ERROR
-            );
-        }
-
-        return JsonResponse::create(
-            $response,
-            Response::HTTP_OK
-        );
-    }
-
     public function handleRequest($data): array
     {
         $signUpUser = SignUpUserCommand::build(
---
+++ tests/Shared/Ui/Api/Rest/AppControllerTest.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Shared\Ui\Api\Rest;
+
+use App\Shared\Domain\Exceptions\DomainException;
+use App\Shared\Ui\Http\Api\Rest\AppController;
+use League\Tactician\CommandBus;
+use League\Tactician\Setup\QuickStart;
+use Monolog\Handler\TestHandler;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DummyAppController extends AppController
+{
+    public function handleRequest($data): ?array
+    {
+        throw DummyException::build('test error message');
+    }
+}
+
+class DummyException extends DomainException
+{
+
+}
+
+class AppControllerTest extends TestCase
+{
+    const TEST_ERROR_MESSAGE = 'test error message';
+    private CommandBus $bus;
+    private Logger $log;
+    private TestHandler $logHandler;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->bus = QuickStart::create([]);
+
+        $this->log = new Logger('testLog');
+        $this->logHandler = new TestHandler();
+        $this->log->pushHandler($this->logHandler);
+    }
+
+    public function testAppControllerWritesErrorLogInCaseOfException()
+    {
+        $data = json_encode([
+            "blah" => ''
+        ]);
+
+        $request = Request::create('/test', 'POST', [], [], [], [], $data);
+
+        $controller = new DummyAppController($this->bus, $this->log);
+
+        $response = $controller->execute($request);
+
+        self::assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        self::assertTrue($this->logHandler->hasErrorThatContains(self::TEST_ERROR_MESSAGE));
+    }
+
+    public function testAppControllerReturnsBadRequestWithInvalidVoidData()
+    {
+        $data = '';
+
+        $request = Request::create('/test', 'POST', [], [], [], [], $data);
+
+        $controller = new DummyAppController($this->bus, $this->log);
+
+        $response = $controller->execute($request);
+
+        self::assertEquals(400, $response->getStatusCode());
+        self::assertEquals('"Empty data or bad json received"', $response->getContent());
+    }
+
+    public function testAppControllerCatchUnexpectedException()
+    {
+        $data = 'TEST_SHOULD_FAIL_WITH_500_EXCEPTION';
+
+        $request = Request::create('/test', 'POST', [], [], [], [], $data);
+
+        $controller = new DummyAppController($this->bus, $this->log);
+
+        $response = $controller->execute($request);
+
+        self::assertEquals(500, $response->getStatusCode());
+        if($_SERVER['APP_DEBUG'] === '1') {
+            self::assertEquals('"Server error:TEST_SHOULD_FAIL_WITH_500_EXCEPTION"', $response->getContent());
+        } else {
+            self::assertEquals('""', $response->getContent());
+        }
+    }
+}
