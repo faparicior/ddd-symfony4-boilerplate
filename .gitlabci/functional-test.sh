48c5d6d29f79a62f56072ccd735036e6cc0a35f7
Fernando Aparicio <faparicior@gmail.com>
2020-05-09 13:19:21 +0200

    cypress testing gitlab

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -9,7 +9,7 @@ cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp /builds/fapariciorteam/ddd-symfony4-boilerplate/.env /var/www/html
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
 ls -al /var/www/html
-#chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html
 echo "# apache"
 apachectl restart
 apachectl status
@@ -17,8 +17,8 @@ apachectl status
 cd /var/www/html
 rm -rf node_modules/cypress
 npm install
-#
-#./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
+
+./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
 
 #export APP_ENV=behat
 #./bin/behat
