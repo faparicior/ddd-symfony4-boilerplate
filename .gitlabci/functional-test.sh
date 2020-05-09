4e4048bb054c9699774a78e568b059bcc4e378c4
Fernando Aparicio <faparicior@gmail.com>
2020-05-09 12:35:31 +0200

    gitlab with cypress

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -2,18 +2,22 @@
 
 export APP_ENV=test
 
-echo "directory"
+echo "# directory"
 pwd
 
 cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
+
+echo "# apache"
 apachectl restart
+apachectl status
+curl 127.0.0.1
 
-cd /var/www/html
-rm -rf node_modules/cypress
-npm install
-
-./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
+#cd /var/www/html
+#rm -rf node_modules/cypress
+#npm install
+#
+#./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
 
 #export APP_ENV=behat
 #./bin/behat
