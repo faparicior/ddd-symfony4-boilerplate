a46b2272b6fcc7ab9357dd12524aa0a41a556360
Fernando Aparicio <faparicior@gmail.com>
2020-05-09 22:03:42 +0200

    cypress testing reporting

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -2,9 +2,6 @@
 
 export APP_ENV=test
 
-echo "# directory"
-pwd
-
 cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp /builds/fapariciorteam/ddd-symfony4-boilerplate/.env /var/www/html
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
@@ -18,11 +15,5 @@ rm -rf node_modules/cypress
 npm install
 
 ./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
-cd cypress/results
-npx marge mochawesome.json
-
-#mv cypress/results /builds/fapariciorteam/ddd-symfony4-boilerplate/cypress
-#mv cypress/videos /builds/fapariciorteam/ddd-symfony4-boilerplate/cypress
-
-#export APP_ENV=behat
-#./bin/behat
+#cd cypress/results
+#npx marge mochawesome.json
