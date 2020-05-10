3d20148bf8053837ee6e65c40c85c620aab6ba30
Fernando Aparicio <faparicior@gmail.com>
2020-05-10 22:04:21 +0200

    cypress testing reporting

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -5,7 +5,6 @@
 cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp /builds/fapariciorteam/ddd-symfony4-boilerplate/.env /var/www/html
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
-ls -al /var/www/html
 chown -R www-data:www-data /var/www/html
 echo "# apache"
 apachectl restart
@@ -14,7 +13,7 @@ apachectl restart
 rm -rf node_modules/cypress
 npm install
 
-npm test
+npm testgitlab
 #./node_modules/.bin/cypress run --headless --spec "cypress/integration/Users/User.js"
 #cd cypress/results
 #npx marge mochawesome.json
