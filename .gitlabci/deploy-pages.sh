93ff9ecb9af3e6e2a0e035504ab7fd1aae6c3f28
Fernando Aparicio <faparicior@gmail.com>
2020-05-11 18:35:11 +0200

    mocha report

--- .gitlabci/deploy-pages.sh
+++ .gitlabci/deploy-pages.sh
@@ -4,3 +4,5 @@ rm public/*
 mv doc/hugo/public/{*,.[!.]*} public
 mkdir public/coverage
 mv build/coverage-report/{*,.[!.]*} public/coverage
+mkdir public/functional-tests
+mv cypress/reports/mochareports/* public/functional-tests
