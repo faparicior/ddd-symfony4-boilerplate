fb1778830051832c4ccbcf3ecef258ed8f6406b9
Fernando Aparicio <faparicior@gmail.com>
2020-05-13 16:02:27 +0200

    docker gitlab

--- docker/gitlab-ci/build.sh
+++ docker/gitlab-ci/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker build . -t webapp_gitlab-ci
+#docker build . -t webapp_gitlab-ci
+docker build -t registry.gitlab.com/fapariciorteam/ddd-symfony4-boilerplate .
+docker push registry.gitlab.com/fapariciorteam/ddd-symfony4-boilerplate
