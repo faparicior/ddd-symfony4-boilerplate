8bd23b85c51e6309cf5d2b0d40b675163bf33327
Fernando Aparicio <faparicior@gmail.com>
2020-05-13 16:05:34 +0200

    docker gitlab

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
+#cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp /builds/fapariciorteam/ddd-symfony4-boilerplate/.env /var/www/html
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
 chown -R www-data:www-data /var/www/html
-echo "# apache"
-apachectl restart
+#echo "# apache"
+#apachectl restart
 
 rm -rf node_modules/cypress
 npm install
