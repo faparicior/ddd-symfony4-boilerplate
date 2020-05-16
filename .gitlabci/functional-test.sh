2ac89dfd4b04d117aa456cf84cdb3d4275935525
Fernando Aparicio <faparicior@gmail.com>
2020-05-16 21:45:50 +0200

    add cypress and rollbar

--- .gitlabci/functional-test.sh
+++ .gitlabci/functional-test.sh
@@ -2,9 +2,9 @@
 
 cp docker/gitlab-ci/httpd.conf /etc/apache2/sites-enabled/000-default.conf
 cp /builds/fapariciorteam/ddd-symfony4-boilerplate/.env.dist /var/www/html/.env
-sed -i 's/SENTRY_DSN_/'$SENTRY_DSN'/g' /var/www/html/.env > /var/www/html/.env
-sed -i 's/ROLLBAR_TOKEN_/'$ROLLBAR_TOKEN'/g' /var/www/html/.env > /var/www/html/.env
-sed -i 's/LOGGY_TOKEN_/'$LOGGY_TOKEN'/g' /var/www/html/.env > /var/www/html/.env
+sed -i 's/SENTRY_DSN_/'$SENTRY_DSN'/g' /var/www/html/.env
+sed -i 's/ROLLBAR_TOKEN_/'$ROLLBAR_TOKEN'/g' /var/www/html/.env
+sed -i 's/LOGGY_TOKEN_/'$LOGGY_TOKEN'/g' /var/www/html/.env
 cp -R /builds/fapariciorteam/ddd-symfony4-boilerplate/* /var/www/html
 chown -R www-data:www-data /var/www/html
 echo "# apache"
